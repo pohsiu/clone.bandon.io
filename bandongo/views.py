d0fd64af64c8a1b386a914840669e98859f16db0
zxcal <willy951753123@gmail.com>
2017-02-21 08:28:55 +0000

    checkExpire at more functions

--- bandongo/views.py
+++ bandongo/views.py
@@ -282,6 +282,7 @@ def logout(request):
 
 @login_required(login_url='/backend/login/')
 def setSchedulePage(request):
+    checkExpire()
     drinks=Drink.objects.all()
     shops=Food.objects.all()
     catalogs=Catalog.objects.filter(foodShop=shops[0])
@@ -289,6 +290,7 @@ def setSchedulePage(request):
 
 @login_required(login_url='/backend/login/')
 def editSchedulePage(request):
+    checkExpire()
     nonFinish=Schedule.objects.filter(finish=False)
     if len(nonFinish)==0:
         return render(request, 'bandongo/backend_editSchedule.html',{})
@@ -318,6 +320,7 @@ def editSchedulePage(request):
 
 @login_required(login_url='/backend/login/')
 def scheduleListPage(request):
+    checkExpire()
     schedules=Schedule.objects.all()
     for i in range(len(schedules)):
         catalogs=Catalog.objects.filter(foodShop=schedules[i].food, choosed=True)
