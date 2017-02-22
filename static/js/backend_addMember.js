44994cc216293f5bb030fdf595fe56b20dea9a81
WeiHung <willy951753123@gmail.com>
2017-02-23 00:11:48 +0800

    same name alert

--- static/js/backend_addMember.js
+++ static/js/backend_addMember.js
@@ -1,13 +1,37 @@
+var categories;
+var members;
+$( document ).ready(function() {
+    var url = "/getCateMem";
+    $.getJSON(url, function(res) {
+        categories=res.categories;
+        members=res.members;
+    });
+});
 function submit() {
     var name=$("#name").val();
-    var phone=$("#phone").val();
-    var email=$("#email").val();
-    var category=$("#category").val();
-    $.post(
-        "/backend/addMember",
-        {name: name, phone: phone, email: email, category: category},
-        function(response) {
-          alert(response);
+    var sameName=false;
+    var category="";
+    for(var i=0; i<members.length; i++) {
+        for(var j=0; j<members[i].length; j++) {
+            if(name==members[i][j].name) {
+                sameName=true;
+                category=categories[i].name;
+                break;
+            }
         }
-    );
+    }
+    if(sameName) {
+        if(confirm("same name with the person in "+category)) {
+            var phone=$("#phone").val();
+            var email=$("#email").val();
+            var category=$("#category").val();
+            $.post(
+                "/backend/addMember",
+                {name: name, phone: phone, email: email, category: category},
+                function(response) {
+                  alert(response);
+                }
+            );
+        }
+    }
 }
