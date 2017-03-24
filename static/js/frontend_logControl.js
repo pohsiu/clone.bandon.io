548075e122955c10a6012b0b4617adb4e8818513
Louis <pohsiu0709@gmail.com>
2017-03-24 00:59:17 +0000

    div button

--- static/js/frontend_logControl.js
+++ static/js/frontend_logControl.js
@@ -3,11 +3,20 @@ function divclick(tableID){
 
     
     var table = document.getElementById("div"+tableID);
-    console.log(table.style.display);
+    // console.log(table.style.display);
+    // var table = $("#div"+tableID);
+    // console.log(table.html());
     if(table.style.display==="none"){
         table.style.display="block";
+        // table.animate({
+        //   top: '0px'
+        // }, 1000);
     }
     else{
         table.style.display="none";
+        // table.animate({
+        //   top: '-100px'
+        // }, 1000);
+        
     }
 }
