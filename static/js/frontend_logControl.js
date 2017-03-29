e0641a2fcecc072bad4e760451fed32736ac8243
weihung <willy951753123@gmail.com>
2017-03-29 12:45:26 +0000

    merge

--- static/js/frontend_logControl.js
+++ static/js/frontend_logControl.js
@@ -1,22 +1,10 @@
 
 function divclick(tableID){
-
-    
-    var table = document.getElementById("div"+tableID);
-    // console.log(table.style.display);
-    // var table = $("#div"+tableID);
+    var table = $("#div"+tableID);
+    var head = $("#head"+tableID);
     // console.log(table.html());
-    if(table.style.display==="none"){
-        table.style.display="block";
-        // table.animate({
-        //   top: '0px'
-        // }, 1000);
-    }
-    else{
-        table.style.display="none";
-        // table.animate({
-        //   top: '-100px'
-        // }, 1000);
-        
-    }
+    table.animate({
+        height:"toggle"    
+    });
+    $('html,body').animate({scrollTop: head.offset().top}, 500);
 }
