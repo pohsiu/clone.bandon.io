eccd25c4cdbd8a16a7394cdf3531bf2374246e4d
weihung <willy951753123@gmail.com>
2017-04-11 04:12:11 +0000

    adjust the catalog change price page

--- static/js/backend_catalogChangePrice.js
+++ static/js/backend_catalogChangePrice.js
@@ -3,7 +3,11 @@ $( document ).ready(function() {
 function submit() {
     if(confirm("Please be sure to adjust the price of all selected catalogs.")) {
         var catalog=$("#catalog").selectpicker('val');
+        var adjust = $('input[name=adjust]:checked').val();
         var price=$("#price").val();
+        if(adjust=="minus") {
+            price=-price;
+        }
         $.post(
             "/backend/catalogChangePrice",
             {catalog: catalog, price: price},
